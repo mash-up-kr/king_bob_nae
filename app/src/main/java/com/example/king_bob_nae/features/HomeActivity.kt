dc02ca53cc46d9163d8589f9f3b5186979fa1b1b
캘리_이지은 <lje4648@afreecatv.com>
2022-07-09 17:44:08 +0900

    main 삭제, home으로 변경

--- app/src/main/java/com/example/king_bob_nae/features/HomeActivity.kt
+++ app/src/main/java/com/example/king_bob_nae/features/HomeActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.king_bob_nae.R
 import com.example.king_bob_nae.base.BaseActivity
-import com.example.king_bob_nae.databinding.ActivityMainBinding
+import com.example.king_bob_nae.databinding.ActivityHomeBinding
 import com.example.king_bob_nae.features.home.presenter.HomeFragment
 import com.example.king_bob_nae.features.kkilog.KkiLogFragment
 import com.example.king_bob_nae.features.recipe.RecipeFragment
 
-class HomeActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
 
     private val homeFragment by lazy { HomeFragment() }
     private val kkiLogFragment by lazy { KkiLogFragment() }
