7d7b371bec2337d973cf0a0024cf3dc6552d1d96
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-07-14 19:41:42 +0900

    TextInputLayout ID 수정
    공식문서 가이드 참고 et -> tf 로 변경

--- app/src/main/java/com/example/king_bob_nae/features/intro/IntroActivity.kt
+++ app/src/main/java/com/example/king_bob_nae/features/intro/IntroActivity.kt
@@ -2,6 +2,6 @@ package com.example.king_bob_nae.features.intro
 
 import com.example.king_bob_nae.R
 import com.example.king_bob_nae.base.BaseActivity
-import com.example.king_bob_nae.databinding.ActivityLoginBinding
+import com.example.king_bob_nae.databinding.ActivitySigninBinding
 
-class IntroActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login)
+class IntroActivity : BaseActivity<ActivitySigninBinding>(R.layout.activity_signin)
---
+++ app/src/main/java/com/example/king_bob_nae/util/Extnesions.kt
@@ -0,0 +1,7 @@
+package com.example.king_bob_nae.util
+
+import com.google.android.material.textfield.TextInputLayout
+
+fun TextInputLayout.isValid(){
+
+}
