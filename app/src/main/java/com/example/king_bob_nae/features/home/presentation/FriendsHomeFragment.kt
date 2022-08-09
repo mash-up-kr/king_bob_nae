3af18c4c43ca86671e229ebde2ed71108547001c
캘리_이지은 <lje4648@afreecatv.com>
2022-08-10 00:12:13 +0900

    [#38] 홈 나/친구 UI 구현

---
+++ app/src/main/java/com/example/king_bob_nae/features/home/presentation/FriendsHomeFragment.kt
@@ -0,0 +1,15 @@
+package com.example.king_bob_nae.features.home.presentation
+
+import android.os.Bundle
+import android.view.View
+import com.example.king_bob_nae.R
+import com.example.king_bob_nae.base.BaseFragment
+import com.example.king_bob_nae.databinding.FragmentFriendsHomeBinding
+
+class FriendsHomeFragment :
+    BaseFragment<FragmentFriendsHomeBinding>(R.layout.fragment_friends_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
