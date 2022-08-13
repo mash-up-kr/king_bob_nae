3a95a1d11b4b21c8bacad1f8c2fdc70a56bfb481
캘리_이지은 <lje4648@afreecatv.com>
2022-08-14 04:35:26 +0900

    [#39] 상세끼록 완료 UI 구현

---
+++ app/src/main/java/com/example/king_bob_nae/features/create/detail/presentaion/DetailKkiLogResultFragment.kt
@@ -0,0 +1,14 @@
+package com.example.king_bob_nae.features.create.detail.presentaion
+
+import android.os.Bundle
+import android.view.View
+import com.example.king_bob_nae.R
+import com.example.king_bob_nae.base.BaseFragment
+import com.example.king_bob_nae.databinding.FragmentDetailKkiLogBinding
+
+class DetailKkiLogResultFragment :
+    BaseFragment<FragmentDetailKkiLogBinding>(R.layout.fragment_detail_kki_log_result) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
