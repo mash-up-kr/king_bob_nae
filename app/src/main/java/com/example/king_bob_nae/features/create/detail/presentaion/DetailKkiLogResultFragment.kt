546c1786bc8b4d752b848a66b8de46ff85ce5f3a
캘리_이지은 <lje4648@afreecatv.com>
2022-08-16 01:43:20 +0900

    [#39] 3dot 메뉴 작업

--- app/src/main/java/com/example/king_bob_nae/features/create/detail/presentaion/DetailKkiLogResultFragment.kt
+++ app/src/main/java/com/example/king_bob_nae/features/create/detail/presentaion/DetailKkiLogResultFragment.kt
@@ -2,13 +2,26 @@ package com.example.king_bob_nae.features.create.detail.presentaion
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.widget.PopupMenu
 import com.example.king_bob_nae.R
 import com.example.king_bob_nae.base.BaseFragment
-import com.example.king_bob_nae.databinding.FragmentDetailKkiLogBinding
+import com.example.king_bob_nae.databinding.FragmentDetailKkiLogResultBinding
 
 class DetailKkiLogResultFragment :
-    BaseFragment<FragmentDetailKkiLogBinding>(R.layout.fragment_detail_kki_log_result) {
+    BaseFragment<FragmentDetailKkiLogResultBinding>(R.layout.fragment_detail_kki_log_result) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        binding.run {
+            ivMore.setOnClickListener { view ->
+                PopupMenu(root.context, view).apply {
+                    menuInflater.inflate(R.menu.menu_kki_log_feed, this.menu)
+                    // setOnMenuItemClickListener {  }
+                }.show()
+            }
+        }
     }
 }
