b599c6e094a22e56cdac930265d2a3ae98b8d818
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-08-16 19:43:12 +0900

    로그인 유스케이스 생성

--- app/src/main/java/com/example/king_bob_nae/features/intro/data/repository/impl/SignInRepositoryImpl.kt
+++ app/src/main/java/com/example/king_bob_nae/features/intro/data/repository/impl/SignInRepositoryImpl.kt
@@ -1,7 +1,7 @@
 package com.example.king_bob_nae.features.intro.data.repository.impl
 
-import com.example.king_bob_nae.features.intro.data.dto.AuthResponseDto
 import com.example.king_bob_nae.features.intro.data.dto.SignInDto
+import com.example.king_bob_nae.features.intro.data.dto.asAccessToken
 import com.example.king_bob_nae.features.intro.data.repository.SignInRepository
 import com.example.king_bob_nae.features.intro.data.service.SignInService
 import javax.inject.Inject
@@ -10,6 +10,11 @@ class SignInRepositoryImpl @Inject constructor(
     private val service: SignInService
 ) : SignInRepository {
 
-    override suspend fun signIn(auth: SignInDto): AuthResponseDto = service.signIn(auth)
+    override suspend fun signIn(email: String, passwd: String): String? {
+        val auth = SignInDto(email, passwd)
+        return kotlin.runCatching {
+            service.signIn(auth).asAccessToken().toString()
+        }.getOrNull()
+    }
 
 }
