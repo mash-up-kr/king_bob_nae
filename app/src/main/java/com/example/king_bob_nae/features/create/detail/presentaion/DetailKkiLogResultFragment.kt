fab58a3f1b719950d595f9a04e38e3380a0602d0
캘리_이지은 <lje4648@afreecatv.com>
2022-08-19 03:08:03 +0900

    [#39] 레시피 지우기 아이콘 추가, 상세 끼록 결과 뷰 연결

--- app/src/main/java/com/example/king_bob_nae/features/create/detail/presentaion/DetailKkiLogResultFragment.kt
+++ app/src/main/java/com/example/king_bob_nae/features/create/detail/presentaion/DetailKkiLogResultFragment.kt
@@ -3,6 +3,7 @@ package com.example.king_bob_nae.features.create.detail.presentaion
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.widget.PopupMenu
+import androidx.navigation.fragment.NavHostFragment
 import com.example.king_bob_nae.R
 import com.example.king_bob_nae.base.BaseFragment
 import com.example.king_bob_nae.databinding.FragmentDetailKkiLogResultBinding
@@ -15,12 +16,21 @@ class DetailKkiLogResultFragment :
     }
 
     private fun initView() {
+        val navHostFragment =
+            requireActivity().supportFragmentManager.findFragmentById(R.id.home_nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
         binding.run {
             ivMore.setOnClickListener { view ->
                 PopupMenu(root.context, view).apply {
                     menuInflater.inflate(R.menu.menu_kki_log_feed, this.menu)
-                    // setOnMenuItemClickListener {  }
+                    setOnMenuItemClickListener { // TODO menu item click 처리
+                        true
+                    }
                 }.show()
+            }
+
+            ivBack.setOnClickListener {
+                navController.popBackStack()
             }
         }
     }
