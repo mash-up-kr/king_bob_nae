f7156d69af59b2925108d0f940525ba3b6ef01d9
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-09-02 23:25:27 +0900

    업로드 완료

--- app/src/main/java/com/example/king_bob_nae/features/create/kkilog/data/service/KkiLogService.kt
+++ app/src/main/java/com/example/king_bob_nae/features/create/kkilog/data/service/KkiLogService.kt
@@ -2,7 +2,6 @@ package com.example.king_bob_nae.features.create.kkilog.data.service
 
 import com.example.king_bob_nae.features.create.kkilog.data.dto.UpLoadResponse
 import okhttp3.MultipartBody
-import okhttp3.RequestBody
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -13,8 +12,8 @@ interface KkiLogService {
     @POST("/logs")
     suspend fun postKkiLog(
         @Part images: List<MultipartBody.Part>,
-        @Part("title") title: RequestBody,
-        @Part("description") description: RequestBody?,
-        @Part("kick") kick: RequestBody?
-    ) : UpLoadResponse
+        @Part title: MultipartBody.Part,
+        @Part description: MultipartBody.Part?,
+        @Part kick: MultipartBody.Part?
+    ): UpLoadResponse
 }
