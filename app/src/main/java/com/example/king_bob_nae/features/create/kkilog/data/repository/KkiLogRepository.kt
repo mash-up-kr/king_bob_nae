b90521210f4cd3e45321b917a2b3b9999ac56ea0
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-09-03 02:32:36 +0900

    끼록작성 완료 시 결과 id 넘겨주기

--- app/src/main/java/com/example/king_bob_nae/features/create/kkilog/data/repository/KkiLogRepository.kt
+++ app/src/main/java/com/example/king_bob_nae/features/create/kkilog/data/repository/KkiLogRepository.kt
@@ -1,7 +1,8 @@
 package com.example.king_bob_nae.features.create.kkilog.data.repository
 
 import com.example.king_bob_nae.features.create.kkilog.data.dto.UpLoadDto
+import com.example.king_bob_nae.features.create.kkilog.data.dto.UpLoadResponse
 
 interface KkiLogRepository {
-    suspend fun postKkiLog(dto: UpLoadDto)
+    suspend fun postKkiLog(dto: UpLoadDto): UpLoadResponse
 }
