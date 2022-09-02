26e9428fd966b7642939f732d7fa7f56e84dd186
ahn seokjoo <tjrwn0716@naver.com>
2022-09-02 16:33:10 +0900

    간끼 결과로 가는 navi 추가 (추후에 더 수정 있을수도 있음~)

--- app/src/main/java/com/example/king_bob_nae/features/create/result/presenter/KkiLogResultFragment.kt
+++ app/src/main/java/com/example/king_bob_nae/features/create/result/presenter/KkiLogResultFragment.kt
@@ -1,8 +1,13 @@
 package com.example.king_bob_nae.features.create.result.presenter
 
+import androidx.navigation.fragment.navArgs
 import com.example.king_bob_nae.R
 import com.example.king_bob_nae.base.BaseFragment
 import com.example.king_bob_nae.databinding.FragmentKkiLogResultBinding
 
 class KkiLogResultFragment :
-    BaseFragment<FragmentKkiLogResultBinding>(R.layout.fragment_kki_log_result)
+    BaseFragment<FragmentKkiLogResultBinding>(R.layout.fragment_kki_log_result) {
+    private val navArgs by navArgs<KkiLogResultFragmentArgs>()
+
+    // 받은 args로 끼록에 대한 정보 받아서 보여주고, 조아요나 삭제 기능 넣을 생각해야함
+}
