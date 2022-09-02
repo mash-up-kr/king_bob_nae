34091562c14e148f9afaa5ec84c4a588b52193ef
ahn seokjoo <tjrwn0716@naver.com>
2022-09-03 03:35:54 +0900

    친구 알람 삭제 친구 끼 텍스트 수정 백스택 수정

--- app/src/main/java/com/example/king_bob_nae/features/home/domain/friendshome/KkilogState.kt
+++ app/src/main/java/com/example/king_bob_nae/features/home/domain/friendshome/KkilogState.kt
@@ -1,6 +1,7 @@
 package com.example.king_bob_nae.features.home.domain.friendshome
 
 data class KkilogState(
+    val id: Int = 0,
     val imageUrl: String = "",
     val title: String = "",
     val type: String = "",
